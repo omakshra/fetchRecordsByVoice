d2ac077d3b425b63e534055fa359e65a291ef480
Internship <Internship@INTERVIEW-02.prophoenix.corp>
2025-06-02 13:28:33 +0530

    Added Flask connectivity test and Razor button

--- FlaskAPI/app.py
+++ FlaskAPI/app.py
@@ -1,10 +1,12 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)  # Allow cross-origin requests from Razor frontend
 
 @app.route('/ping', methods=['GET'])
 def ping():
-    return jsonify(message="Flask is alive!")
+    return jsonify({"message": "Flask is connected to Razor!"})
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    app.run(debug=True)
