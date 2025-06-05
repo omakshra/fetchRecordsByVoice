04ee641a5ab4396c6079c5729879c32e7ec87b77
Internship <Internship@INTERVIEW-02.prophoenix.corp>
2025-06-05 15:10:36 +0530

    connected form to sql

---
+++ frontend/Migrations/20250605092341_MakeCitizenFieldsRequired.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace frontend.Migrations
+{
+    /// <inheritdoc />
+    public partial class MakeCitizenFieldsRequired : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
